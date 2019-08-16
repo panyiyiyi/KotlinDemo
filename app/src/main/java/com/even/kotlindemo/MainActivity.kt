30a3dec2300a67cd676cf93ec5b48986c063e51f
even <1171867212@qq.com>
2019-08-16 17:36:10 +0800

    update

--- app/src/main/java/com/even/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/even/kotlindemo/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.even.kotlindemo
 
+import android.view.MenuItem
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.even.common.base.BaseLazyFragment
 import com.even.kt_common.base.BaseKtActivity
 import com.even.kt_common.router.ARouterManagerUtils
@@ -7,6 +10,7 @@ import com.even.kt_common.router.ARouterPath
 import com.even.kt_common.router.provider.IArticleProvider
 import com.even.kt_common.router.provider.IHomeProvider
 import com.even.kt_common.router.provider.IProjectProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseKtActivity() {
     var fragmentLists = mutableListOf<BaseLazyFragment>()
@@ -22,7 +26,29 @@ class MainActivity : BaseKtActivity() {
 
     override fun initView() {
         initFragment()
+
+        bottomView.setOnNavigationItemSelectedListener { setBottomNavigationViewItemSelect(it) }
+
     }
+
+    private fun setBottomNavigationViewItemSelect(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.bottom_home -> {
+                viewPager.currentItem = 0
+                true
+            }
+            R.id.bottom_article -> {
+                viewPager.currentItem = 1
+                true
+            }
+            R.id.bottom_project -> {
+                viewPager.currentItem = 2
+                true
+            }
+            else -> false
+        }
+    }
+
 
     private fun initFragment() {
         homeProvider = ARouterManagerUtils.getProvider(ARouterPath.HOME_SERVICE_PATH)
